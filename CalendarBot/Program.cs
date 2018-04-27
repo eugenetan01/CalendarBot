0817b19464d86e7e6a9a221a3e42948b9eb1b4a4
eugenetan2015 <eugene.tan.2015@sis.smu.edu.sg>
2018-04-27 21:08:20 +0800

    Removed client secret and changed the code to return daily events.

--- CalendarBot/Program.cs
+++ CalendarBot/Program.cs
@@ -12,89 +12,86 @@ using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 
-namespace CalendarQuickstart
+namespace CalendarBot
 {
-	class Program
-	{
-        private static readonly TelegramBotClient bot = new TelegramBotClient("560256367:AAHJ2SGUS82VlqzaR53o9Oc7I8a1k1l3jtQ");
-		static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
-		static string ApplicationName = "Google Calendar API .NET Quickstart";
-		static UserCredential credential;
-        static Boolean calendar_retrieved;
+    class Program
+    {
+        private static readonly TelegramBotClient bot = new TelegramBotClient("Enter code here");
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "Google Calendar API .NET Quickstart";
+        static UserCredential credential;
         static Events events;
         static CalendarService service;
 
-		static void Main(string[] args)
+        static void Main(string[] args)
         {
-			using (var stream =
-				new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
-			{
-				string credPath = System.Environment.GetFolderPath(
-					System.Environment.SpecialFolder.Personal);
-				credPath = Path.Combine(credPath, ".credentials/calendar-dotnet-quickstart.json");
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/calendar-dotnet-quickstart.json");
 
-				credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-					GoogleClientSecrets.Load(stream).Secrets,
-					Scopes,
-					"user",
-					CancellationToken.None,
-					new FileDataStore(credPath, true)).Result;
-				Console.WriteLine("Credential file saved to: " + credPath);
-			}
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
 
-			bot.OnMessage += bot_OnMessage;
+            bot.OnMessage += bot_OnMessage;
             bot.StartReceiving();
             Console.ReadLine();
             bot.StopReceiving();
         }
 
-        private static void get_calendar(){
-			service = new CalendarService(new BaseClientService.Initializer()
-			{
-				HttpClientInitializer = credential,
-				ApplicationName = ApplicationName,
-			});
+        private static void get_calendar()
+        {
+            service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
 
-			
-		}
+
+        }
 
         private static void bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
-            if  (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.TextMessage)
+            string text = e.Message.Text;
+            Console.WriteLine(text);
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.TextMessage && e.Message.Text.Equals("/getCal"))
             {
-                if(!calendar_retrieved){
-                    get_calendar();
-                }
-
-				// List events.
+				get_calendar();
 				EventsResource.ListRequest request = service.Events.List("primary");
 				request.TimeMin = DateTime.Now;
+				request.TimeMax = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 23, 59, 59);
 				request.ShowDeleted = false;
 				request.SingleEvents = true;
 				request.MaxResults = 10;
 				request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
-				calendar_retrieved = true;
 				events = request.Execute();
-                bot.SendTextMessageAsync(e.Message.Chat.Id, "Upcoming events:");
-                if (events.Items != null && events.Items.Count > 0)
-                {
-                    foreach (var eventItem in events.Items)
-                    {
-                        string when = eventItem.Start.DateTime.ToString();
-                        if (String.IsNullOrEmpty(when))
-                        {
-                            when = eventItem.Start.Date;
-                        }
-                        //Console.WriteLine("{0} ({1})", eventItem.Summary, when);
-                        bot.SendTextMessageAsync(e.Message.Chat.Id, eventItem.Summary + when);
-                    }
-                }
-                else
-                {
-                    bot.SendTextMessageAsync(e.Message.Chat.Id, "No upcoming events found.");
-                }
-                Console.Read();
+
+				if (events.Items != null && events.Items.Count > 0)
+				{
+                    for (int i = 0; i <= events.Items.Count()-1; i++)
+					{
+                        var eventItem = events.Items[i];
+						string when = eventItem.Start.DateTime.ToString();
+						if (String.IsNullOrEmpty(when))
+						{
+							when = eventItem.Start.Date;
+						}
+                        bot.SendTextMessageAsync(e.Message.Chat.Id, eventItem.Summary + " " + when + " " + eventItem.Location);
+					}
+				}
+				else
+					bot.SendTextMessageAsync(e.Message.Chat.Id, "No upcoming events found.");
             }
+            else
+                bot.SendTextMessageAsync(e.Message.Chat.Id, "Please enter the correct command.");
+        
         }
-	}
+    }
 }
